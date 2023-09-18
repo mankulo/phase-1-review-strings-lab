48b7685670c689817d2c23ad821de11b5eb28930
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-09-18 15:19:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const currentUser = 'Grace Hopper';
+const currentUser = "Grace Hopper";
 
-const welcomeMessage = 'Welcome to Flatbook, Grace Hopper!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, GRACE HOPPER!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = "Welcome, G!";
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
